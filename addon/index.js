eb3f8802dabf959fdf735764ee7cdd6a00fde80d
Max Fierke <max@maxfierke.com>
2018-03-14 23:52:12 -0500

    Add index default export

---
+++ addon/index.js
@@ -0,0 +1,9 @@
+import retryable from './retryable';
+import defineModifer from './define-modifier';
+import DelayPolicy from './policies/delay';
+
+export default {
+  retryable,
+  defineModifer,
+  DelayPolicy
+}
